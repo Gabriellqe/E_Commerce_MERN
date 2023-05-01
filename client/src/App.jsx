29b998a59b2884b9e11c1f647cca97082c92732f
Gabriel Luque <98136436+Gabriellqe@users.noreply.github.com>
2023-04-30 23:06:35 -0400

    Added Stripe payments features

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,5 @@
+import { useRoutes } from "react-router-dom";
+
 import Cart from "./pages/Cart";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -6,11 +8,15 @@ import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 
 const App = () => {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
-};
+  const routes = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "/products/:category", element: <ProductList /> },
+    { path: "/product/:id", element: <Product /> },
+    { path: "/cart", element: <Cart /> },
+    { path: "/login", element: <Login /> },
+    { path: "/register", element: <Register /> },
+  ]);
 
+  return routes;
+};
 export default App;
